e85d2704064252fd38aa5800282c91d128e51458
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2025-01-06 16:46:05 -0500

    Update keymap for comment plugin

--- lua/daharux/lazy/comment.lua
+++ lua/daharux/lazy/comment.lua
@@ -1,6 +1,11 @@
 return {
 	"numToStr/Comment.nvim",
 	config = function()
-		require('Comment').setup()
+		require('Comment').setup({
+				toggler = {
+						line = "t;",
+						block = "tl;"
+				}
+		})
 	end,
 }
