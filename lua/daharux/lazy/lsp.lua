cef20924765e3846077177271f23e90fb11cd2a7
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2025-10-12 13:33:01 -0500

    Update config with newest API

--- lua/daharux/lazy/lsp.lua
+++ lua/daharux/lazy/lsp.lua
@@ -1,90 +1,96 @@
 return {
-		'neovim/nvim-lspconfig',
-		dependencies = {
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'hrsh7th/nvim-cmp'},
-				{'williamboman/mason.nvim'},
-				{'williamboman/mason-lspconfig.nvim'},
-		},
-		config = function () 
-				local cmp = require('cmp')
-				vim.opt.signcolumn = 'yes'
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+  },
+  config = function()
+    local cmp = require('cmp')
+    vim.opt.signcolumn = 'yes'
 
-				-- Add cmp_nvim_lsp capabilities settings to lspconfig
-				-- This should be executed before you configure any language server
-				local lspconfig_defaults = require('lspconfig').util.default_config
-				lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-				'force',
-				lspconfig_defaults.capabilities,
-				require('cmp_nvim_lsp').default_capabilities()
-				)
+    -- Add cmp_nvim_lsp capabilities to default LSP capabilities
+    -- This provides better completion support with LSP
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-				-- This is where you enable features that only work
-				-- if there is a language server active in the file
-				vim.api.nvim_create_autocmd('LspAttach', {
-						desc = 'LSP actions',
-						callback = function(event)
-								local opts = {buffer = event.buf}
+    -- Configure TypeScript language server using the new vim.lsp.config API
+    vim.lsp.config.tsserver = {
+      filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "mjs" },
+      capabilities = capabilities,
+    }
 
-								vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-								vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-								vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-								vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-								vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-								vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-								vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-								vim.keymap.set({'n', 'x'}, '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-								vim.keymap.set('n', '<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-								vim.keymap.set('n', '<leader>vrr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-								vim.keymap.set('n', '<leader>vca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-								vim.keymap.set('n', '<leader>vd', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-								vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-								vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-						end,
-				})
+    -- This is where you enable features that only work
+    -- if there is a language server active in the file
+    vim.api.nvim_create_autocmd('LspAttach', {
+      desc = 'LSP actions',
+      callback = function(event)
+        local opts = { buffer = event.buf }
 
-				require('mason').setup({})
-				require('mason-lspconfig').setup({
-						handlers = {
-								function(server_name)
-										require('lspconfig')[server_name].setup({})
-								end,
-						},
-				})
-				local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set('n', '<leader>vrr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', '<leader>vca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        vim.keymap.set('n', '<leader>vd', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+      end,
+    })
 
-				cmp.setup({
-						snippet = {
-								expand = function(args)
-										require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-								end,
-						},
-						mapping = cmp.mapping.preset.insert({
-								['<S-tab>'] = cmp.mapping.select_prev_item(cmp_select),
-								['<tab>'] = cmp.mapping.select_next_item(cmp_select),
-								['<enter>'] = cmp.mapping.confirm({ select = true }),
-								["<C-Space>"] = cmp.mapping.complete(),
-								
-						}),
-						sources = cmp.config.sources({
-								{ name = 'nvim_lsp' },
-								{ name = 'luasnip' }, -- For luasnip users.
-						}, {
-								{ name = 'buffer' },
-						})
-				})
+    require('mason').setup({})
 
-				vim.diagnostic.config({
-						-- update_in_insert = true,
-						float = {
-								focusable = false,
-								style = "minimal",
-								border = "rounded",
-								source = "always",
-								header = "",
-								prefix = "",
-						},
-				})
-		end
+    -- Setup Mason to use vim.lsp.config instead of lspconfig
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          -- For each server installed via Mason, set it up using vim.lsp.config
+          -- This automatically detects the server_name and uses appropriate settings
+          vim.lsp.config[server_name] = {
+            capabilities = capabilities,
+          }
+        end,
+      },
+    })
 
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)           -- For `luasnip` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<S-tab>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<tab>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<enter>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },         -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      })
+    })
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+  end
 }
