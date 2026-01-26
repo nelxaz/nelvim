21df260f88420973dbb9436927db5fce385fe1a5
Nelson <nelsonx.zambrano@gmail.com>
2026-01-26 14:09:40 -0500

    Update config

--- lua/daharux/lazy/init.lua
+++ lua/daharux/lazy/init.lua
@@ -1,7 +1,7 @@
 return {
-  require("daharux.lazy.lsp"),
-  require("daharux.lazy.leap"),
-  require("daharux.lazy.conform"),
-  require("daharux.lazy.lint"),
-  require("daharux.lazy.treesitter"),
+  require("nelvim.lua.daharux.lazy.lsp"),
+  require("nelvim.lua.daharux.lazy.leap"),
+  require("nelvim.lua.daharux.lazy.conform"),
+  require("nelvim.lua.daharux.lazy.lint"),
+  require("nelvim.lua.daharux.lazy.treesitter"),
 }
