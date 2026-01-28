efbb35bf1ec141345d08786a37a547efd8c0f035
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2026-01-28 09:13:11 -0500

    update formatter used

--- lua/daharux/lazy/conform.lua
+++ lua/daharux/lazy/conform.lua
@@ -4,15 +4,15 @@ return {
     opts.formatters_by_ft = opts.formatters_by_ft or {}
     local formatters = opts.formatters_by_ft
 
-    formatters.javascript = { "prettier" }
-    formatters.javascriptreact = { "prettier" }
-    formatters.typescript = { "prettier" }
-    formatters.typescriptreact = { "prettier" }
-    formatters.json = { "prettier" }
-    formatters.jsonc = { "prettier" }
-    formatters.css = { "prettier" }
-    formatters.scss = { "prettier" }
-    formatters.html = { "prettier" }
+    formatters.javascript = { "prettierd" }
+    formatters.javascriptreact = { "prettierd" }
+    formatters.typescript = { "prettierd" }
+    formatters.typescriptreact = { "prettierd" }
+    formatters.json = { "prettierd" }
+    formatters.jsonc = { "prettierd" }
+    formatters.css = { "prettierd" }
+    formatters.scss = { "prettierd" }
+    formatters.html = { "prettierd" }
 
     opts.format_on_save = opts.format_on_save or { timeout_ms = 500, lsp_fallback = true }
 
