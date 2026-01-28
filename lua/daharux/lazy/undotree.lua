afb3686bbcdb05498c3ed78609600447c6a45e4c
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2026-01-28 09:13:30 -0500

    remove unused formatter trigger

--- lua/daharux/lazy/undotree.lua
+++ lua/daharux/lazy/undotree.lua
@@ -1,10 +1,3 @@
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-augroup("__formatter__", { clear = true })
-autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
-})
 return {
 	'mbbill/undotree',
 	config = function()
