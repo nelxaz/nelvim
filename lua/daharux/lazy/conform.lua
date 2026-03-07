1bbd5bc6b82eaf68a39f6a0ac95e3f2b77f70126
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2026-03-07 15:30:24 -0500

    update prettier for oxlint

--- lua/daharux/lazy/conform.lua
+++ lua/daharux/lazy/conform.lua
@@ -4,15 +4,15 @@ return {
     opts.formatters_by_ft = opts.formatters_by_ft or {}
     local formatters = opts.formatters_by_ft
 
-    formatters.javascript = { "prettierd" }
-    formatters.javascriptreact = { "prettierd" }
-    formatters.typescript = { "prettierd" }
-    formatters.typescriptreact = { "prettierd" }
-    formatters.json = { "prettierd" }
-    formatters.jsonc = { "prettierd" }
-    formatters.css = { "prettierd" }
-    formatters.scss = { "prettierd" }
-    formatters.html = { "prettierd" }
+    formatters.javascript = { "oxlint" }
+    formatters.javascriptreact = { "oxlint" }
+    formatters.typescript = { "oxlint" }
+    formatters.typescriptreact = { "oxlint" }
+    formatters.json = { "oxlint" }
+    formatters.jsonc = { "oxlint" }
+    formatters.css = { "oxlint" }
+    formatters.scss = { "oxlint" }
+    formatters.html = { "oxlint" }
 
     opts.format_on_save = opts.format_on_save or { timeout_ms = 500, lsp_fallback = true }
 
