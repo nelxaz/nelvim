46bd404bbe315efbfca6202271074c1477dbf524
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2026-03-07 15:17:59 -0500

    update lsp lua file for section configuration

--- lua/daharux/lazy/lsp.lua
+++ lua/daharux/lazy/lsp.lua
@@ -1,106 +1,159 @@
+local function safe_require(module)
+  local ok, loaded = pcall(require, module)
+  if not ok then
+    return nil
+  end
+
+  return loaded
+end
+
+local function get_capabilities()
+  local cmp_lsp = safe_require("cmp_nvim_lsp")
+  if not cmp_lsp then
+    return vim.lsp.protocol.make_client_capabilities()
+  end
+
+  return cmp_lsp.default_capabilities()
+end
+
+local function setup_lsp_keymaps()
+  vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP actions",
+    callback = function(event)
+      local opts = { buffer = event.buf }
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+      vim.keymap.set({ "n", "x" }, "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
+      vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    end,
+  })
+end
+
+local function setup_diagnostics()
+  vim.diagnostic.config({
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  })
+end
+
+local function setup_completion()
+  local cmp = safe_require("cmp")
+  if not cmp then
+    return
+  end
+
+  local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        local luasnip = safe_require("luasnip")
+        if luasnip then
+          luasnip.lsp_expand(args.body)
+        end
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<S-tab>"] = cmp.mapping.select_prev_item(cmp_select),
+      ["<tab>"] = cmp.mapping.select_next_item(cmp_select),
+      ["<enter>"] = cmp.mapping.confirm({ select = true }),
+      ["<C-Space>"] = cmp.mapping.complete(),
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    }, {
+      { name = "buffer" },
+    }),
+  })
+end
+
+local function setup_servers(capabilities)
+  local servers = {
+    tsserver = {
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+        "mjs",
+      },
+    },
+    rust_analyzer = {
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+          },
+          check = {
+            command = "clippy",
+          },
+          procMacro = {
+            enable = true,
+          },
+        },
+      },
+    },
+  }
+
+  local mason = safe_require("mason")
+  local mason_lspconfig = safe_require("mason-lspconfig")
+  if not mason or not mason_lspconfig then
+    return
+  end
+
+  mason.setup({})
+
+  mason_lspconfig.setup({
+    handlers = {
+      function(server_name)
+        local server_config = servers[server_name] or {}
+
+        vim.lsp.config[server_name] = vim.tbl_deep_extend("force", server_config, {
+          capabilities = capabilities,
+        })
+      end,
+    },
+  })
+end
+
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
   dependencies = {
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'mason-org/mason.nvim' },
-    { 'mason-org/mason-lspconfig.nvim' },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/nvim-cmp" },
+    { "mason-org/mason.nvim" },
+    { "mason-org/mason-lspconfig.nvim" },
   },
   config = function()
-    local ok_cmp, cmp = pcall(require, "cmp")
-    if not ok_cmp then
-      return
-    end
-    vim.opt.signcolumn = 'yes'
+    vim.opt.signcolumn = "yes"
 
-    -- Add cmp_nvim_lsp capabilities to default LSP capabilities
-    -- This provides better completion support with LSP
-    local ok_cmp_lsp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-    if not ok_cmp_lsp then
-      return
-    end
-    local capabilities = cmp_lsp.default_capabilities()
+    local capabilities = get_capabilities()
 
-    -- Configure TypeScript language server using the new vim.lsp.config API
-    vim.lsp.config.tsserver = {
-      filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "mjs" },
-      capabilities = capabilities,
-    }
-
-    -- This is where you enable features that only work
-    -- if there is a language server active in the file
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = { buffer = event.buf }
-
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<leader>vrn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set('n', '<leader>vrr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', '<leader>vca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-        vim.keymap.set('n', '<leader>vd', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-        vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-        vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-      end,
-    })
-
-    require('mason').setup({})
-
-    -- Setup Mason to use vim.lsp.config instead of lspconfig
-    require('mason-lspconfig').setup({
-      handlers = {
-        function(server_name)
-          -- For each server installed via Mason, set it up using vim.lsp.config
-          -- This automatically detects the server_name and uses appropriate settings
-          vim.lsp.config[server_name] = {
-            capabilities = capabilities,
-          }
-        end,
-      },
-    })
-
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          local ok_luasnip, luasnip = pcall(require, "luasnip")
-          if ok_luasnip then
-            luasnip.lsp_expand(args.body)
-          end
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<S-tab>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<tab>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<enter>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },         -- For luasnip users.
-      }, {
-        { name = 'buffer' },
-      })
-    })
-
-    vim.diagnostic.config({
-      -- update_in_insert = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-      },
-    })
-  end
+    setup_lsp_keymaps()
+    setup_servers(capabilities)
+    setup_completion()
+    setup_diagnostics()
+  end,
 }
