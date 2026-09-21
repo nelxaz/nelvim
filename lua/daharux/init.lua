e1136f1a22289dd699010701cc15755ad6e6be8a
Xavier Zambrano <nelson.zambrano@simprogroup.com>
2026-09-21 16:48:22 -0500

    update changes for working on other computers with vscode

--- lua/daharux/init.lua
+++ lua/daharux/init.lua
@@ -1,5 +1,14 @@
 require("daharux.remap")
 require("daharux.set")
+-- Temporary bridge for Neovim 0.11+ breaking changes
+if not vim.F then
+  vim.F = {
+    if_nil = function(val, default)
+      if val == nil then return default end
+      return val
+    end
+  }
+end
 
 -- Solo inicializa lazy.nvim si aún no fue configurado por otra capa
 if not vim.g.lazy_did_setup then
